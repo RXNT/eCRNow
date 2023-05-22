d79d3dc7b23bfe333500c406b6617ceba54f6352
Ulugbek Normatov <124608808+unormatov-rxnt@users.noreply.github.com>
2023-05-22 13:38:20 -0400

    Extract KeyVault values from application properties command fixed

--- .circleci/env_settings.py
+++ .circleci/env_settings.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 # Installation
+# pip3 install python-dotenv
 
-# usage: env.py [-h] --filename APP_PROPS_FILE [APP_PROPS_FILE ...] [--output [JSON_OUTFILE]]
+# usage: env.py [-h] --filename ENV_FILE [ENV_FILE ...] [--output [JSON_OUTFILE]]
 
 # Process some integers.
 
@@ -20,39 +21,32 @@
 
 import sys, getopt, json
 import argparse
+from dotenv import dotenv_values
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 
-parser.add_argument('--filename', dest='app_props_file', nargs='+', default=sys.stdin, required=True, help='The .env file with key-value pairs.')
+parser.add_argument('--filename', dest='env_file', nargs='+', default=sys.stdin, required=True, help='The .env file with key-value pairs.')
 
 parser.add_argument('--output', dest='json_outfile', nargs='?', default='env.json', help='The name of your output json file.')
 
 args = parser.parse_args()
 
+def clean_non_ms_keyvault_values(config):
+    for key in config.copy().keys():
+        if(not str(config[key]).startswith("@Microsoft.KeyVault")):
+            config.pop(key, None)
 
-def convert_app_props_file(app_props_filename):
-    print('Loading file: ', app_props_filename)
+    return config
 
-    props_file = open(app_props_filename)
-    prop_lines = props_file.read().splitlines()
-    props_file.close()
-
-    config = {}
-    for i, line in enumerate(prop_lines):
-
-        if (line.startswith("#")):
-            continue
-
-        key_val = line.split("=", 1)
-        key = key_val[0].strip()
-        val = key_val[1].strip()
-
-        if (not val.startswith("@Microsoft.KeyVault")):
-            config[key] = val
+def convert_env_file(env_filename):
+    print('Loading file: ', args.env_file[0])
+    config = clean_non_ms_keyvault_values(
+        dotenv_values(env_filename)
+    )
 
     with open(args.json_outfile, 'w', encoding='utf-8') as f:
         json.dump(config, f, ensure_ascii=False, indent=4)
         print('Wrote json to ' + args.json_outfile)
-
+        
 if __name__ == "__main__":
-    convert_app_props_file(args.app_props_file[0])
+    convert_env_file(args.env_file[0])
